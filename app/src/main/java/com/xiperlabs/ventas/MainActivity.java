dc9a7a8c7e2e8db647d68721b05087b5bab7fdc9
edgar <xiperlabs>
2019-12-18 23:32:35 -0600

    actualizacion

--- app/src/main/java/com/xiperlabs/ventas/MainActivity.java
+++ app/src/main/java/com/xiperlabs/ventas/MainActivity.java
@@ -3,7 +3,6 @@ package com.xiperlabs.ventas;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.view.Gravity;
 import android.view.View;
 import androidx.navigation.NavController;
